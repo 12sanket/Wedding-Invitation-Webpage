5977c7e7a7abbc8b8e03e588863d0c49c16f751f
unknown <12sanketkumbhar@gmail.com>
2020-06-13 16:52:58 +0530

    Added counter functionality

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("June 13, 2020 17:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "Happily Married";
+  }
+}, 1000);
